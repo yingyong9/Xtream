2496eeff62dfa9926b63625b8e9641b19e6a5050
masterUNG <masterUng@abc.com>
2023-09-10 20:15:55 +0700

    v49

---
+++ lib/pages/list_live.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:xstream/pages/watch_live_video.dart';
+
+class ListLive extends StatelessWidget {
+  const ListLive({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: WatchLiveVideo(),);
+  }
+}
