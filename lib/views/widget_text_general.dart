3bddf75b47a7e43d19d4d62090e75dcabad806ad
masterUNG <masterUng@abc.com>
2023-11-05 20:18:08 +0700

    update

--- lib/views/widget_text_general.dart
+++ lib/views/widget_text_general.dart
@@ -1,25 +1,24 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
-class WidgetText extends StatelessWidget {
-  const WidgetText({
-    Key? key,
+class WidgetTextGeneral extends StatelessWidget {
+  const WidgetTextGeneral({
+    super.key,
     required this.data,
     this.textStyle,
-    this.maxLines,
-  }) : super(key: key);
+    
+  });
 
   final String data;
   final TextStyle? textStyle;
-  final int? maxLines;
+ 
 
   @override
   Widget build(BuildContext context) {
     return Text(
       data,
       style: textStyle,
-      maxLines: maxLines,
-      overflow: TextOverflow.ellipsis,
+     
     );
   }
 }
